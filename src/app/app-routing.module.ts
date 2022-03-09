8643d0d35add0f372ec4eac9bce9e700976fd29a
Simple Dimple John <simpledimplejohn@gmail.com>
2022-03-09 11:37:34 -0800

    add boostrapped nav bar

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,9 +1,13 @@
+import { SearchComponent } from './components/search/search.component';
+import { AddalbumComponent } from './components/addalbum/addalbum.component';
 import { HomeComponent } from './components/home/home.component';
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
 const routes: Routes = [
-  {path : "", component: HomeComponent}
+  {path : "", component: HomeComponent},
+  {path : "addalbum", component: AddalbumComponent},
+  {path : "search", component: SearchComponent}
 ];
 
 @NgModule({
