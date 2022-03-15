36285758f8c5149cc143ef4531a2741bdf078b2f
Simple Dimple John <simpledimplejohn@gmail.com>
2022-03-15 08:50:33 -0700

    add album service findAllAlbums method

---
+++ src/app/service/album.service.ts
@@ -0,0 +1,39 @@
+import { Observable, throwError, catchError } from 'rxjs';
+import { HttpClient, HttpHeaders, HttpErrorResponse } from '@angular/common/http';
+import { localUrl } from 'src/environments/environment';
+import { Injectable } from '@angular/core';
+import { Album } from '../models/album';
+
+const url = `${localUrl}album`
+
+@Injectable({
+  providedIn: 'root'
+})
+export class AlbumService {
+
+  constructor(private http: HttpClient) { }
+
+  httpOptions = {
+    headers: new HttpHeaders({ 'Content-Type': 'application/json'})
+  }
+
+  findAllAlbums(): Observable<Album[]> {
+    return this.http.get<Album[]>(url)
+      .pipe(catchError(this.handleError));
+  }
+
+  private handleError(httpError: HttpErrorResponse) {
+
+    if (httpError.error instanceof ErrorEvent) {
+      console.log('An error occurred: ', httpError.error.message)
+    } else {
+      console.error(`
+        Backend returned code ${httpError.status},
+        body was: ${httpError.error}
+      `);
+    }
+
+    // throwError is an Observable object from RxJS Reactive Extensions for JavaScript
+    return throwError(() => new Error('Something really bad happened, please try again later'));
+  }
+}
